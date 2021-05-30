3a06c7f9bb9edaa54f7523a864657d149e8fa5a3
hiagoLF <hiagolof@gmail.com>
2021-05-30 10:12:00 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+# React Google Maps Clustered
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<img src='./prints/print_1.png'></img>
+<img src='./prints/print_2.png'></img>
 
-## Available Scripts
+##### Este repositório contém um projeto de exemplo de como implementar um mapa do google maps. Para executar siga as instruções a seguir...
 
-In the project directory, you can run:
+1. Primeiro você precisa estar com o Node.js instalado em sua máquina e de preferência, com o yarn instalado também
+2. Para que este projeto funcione você precisa ir até o arquivo `.env.example` e renomear para `.env`
+3. A seguir, você precisa registrar uma chave de api do google ou utilizar uma já registrada. Esta chave deve possuir o serviço do google maps ativado.
+4. Cole a chave dentro do arquivo `.env` como valor para `REACT_APP_GOOGLE_KEY`
+5. Agora você pode instalar as dependências deste projeto com o comando...
+   `yarn install`
+6. Com todas as dependências instaladas, você já pode executar o modo de desenvolvimento do projeto com o comando...
+   `yarn start`
 
-### `yarn start`
+##### Gerar uma versão de produção:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Parar gerar um código mimificado que é destinado a produção você só precisa rodar o seguinte comando
+`yarn build`
+Será gerada uma pasta `./build` contendo o projeto compilado.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import useSwr from "swr";
+import GoogleMapReact from "google-map-react";
+import useSupercluster from "use-supercluster";
+
+import "./App.css";
+
+import crimeIcon from "./custody.png";
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
+const Marker = ({ children }) => children;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // setup map
+   const mapRef = useRef();
+
+   // load and prepare data
+   const url =
+      "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+   const { data, error } = useSwr(url, { fetcher });
+   const crimes = data && !error ? data : [];
+   const points = crimes.map((crime) => ({
+      type: "Feature",
+      properties: {
+         cluster: false,
+         crimeId: crime.id,
+         category: crime.category,
+      },
+      geometry: {
+         type: "Point",
+         coordinates: [
+            parseFloat(crime.location.longitude),
+            parseFloat(crime.location.latitude),
+         ],
+      },
+   }));
+
+   // get map bounds
+   const [bounds, setBounds] = useState(null);
+   const [zoom, setZoom] = useState(10);
+   const [pointsInsideBounds, setPointsInsideBounds] = useState([]);
+
+   // get clusters
+   const { clusters, supercluster } = useSupercluster({
+      points,
+      bounds,
+      zoom,
+      options: { radius: 75, maxZoom: 20 },
+   });
+
+   useEffect(() => {
+      const pointsInsideBounds = points.filter((point) => {
+         const [longitude, latitude] = point.geometry.coordinates;
+         const [nwlng, selat, selng, nwlat] = bounds;
+         if (
+            nwlng < longitude &&
+            selat < latitude &&
+            selng > longitude &&
+            nwlat > latitude
+         ) {
+            return true;
+         } else {
+            return false;
+         }
+      });
+      setPointsInsideBounds(pointsInsideBounds.slice(0, 100));
+   }, [bounds]);
+
+   // return map
+   return (
+      <div style={{ height: "100vh", width: "100%", display: "flex" }}>
+         <div style={{ height: "100%", width: "70%" }}>
+            <GoogleMapReact
+               bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
+               defaultCenter={{ lat: 52.6376, lng: -1.135171 }}
+               defaultZoom={10}
+               yesIWantToUseGoogleMapApiInternals
+               onGoogleApiLoaded={({ map }) => {
+                  mapRef.current = map;
+               }}
+               onChange={({ zoom, bounds }) => {
+                  setZoom(zoom);
+                  setBounds([
+                     bounds.nw.lng,
+                     bounds.se.lat,
+                     bounds.se.lng,
+                     bounds.nw.lat,
+                  ]);
+               }}
+            >
+               {clusters.map((cluster) => {
+                  const [longitude, latitude] = cluster.geometry.coordinates;
+                  const { cluster: isCluster, point_count: pointCount } =
+                     cluster.properties;
+
+                  if (isCluster) {
+                     return (
+                        <Marker
+                           key={`cluster-${cluster.id}`}
+                           lat={latitude}
+                           lng={longitude}
+                        >
+                           <div
+                              className="cluster-marker"
+                              style={{
+                                 width: `${
+                                    10 + (pointCount / points.length) * 20
+                                 }px`,
+                                 height: `${
+                                    10 + (pointCount / points.length) * 20
+                                 }px`,
+                              }}
+                              onClick={() => {
+                                 const expansionZoom = Math.min(
+                                    supercluster.getClusterExpansionZoom(
+                                       cluster.id
+                                    ),
+                                    20
+                                 );
+                                 mapRef.current.setZoom(expansionZoom);
+                                 mapRef.current.panTo({
+                                    lat: latitude,
+                                    lng: longitude,
+                                 });
+                              }}
+                           >
+                              {pointCount}
+                           </div>
+                        </Marker>
+                     );
+                  }
+
+                  return (
+                     <Marker
+                        key={`crime-${cluster.properties.crimeId}`}
+                        lat={latitude}
+                        lng={longitude}
+                     >
+                        <button className="crime-marker">
+                           <img src={crimeIcon} alt="crime doesn't pay" />
+                        </button>
+                     </Marker>
+                  );
+               })}
+            </GoogleMapReact>
+         </div>
+         <ul style={{ width: "10%", overflow: "auto" }} className="results">
+            {pointsInsideBounds.map((point) => (
+               <li>{JSON.stringify(point.properties.crimeId)}</li>
+            ))}
+         </ul>
+      </div>
+   );
 }
 
 export default App;
